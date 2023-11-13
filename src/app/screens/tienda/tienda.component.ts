f53078ba010a2021c9c51acb153ca4cced935d53
José Luis ELizondo <elizondostudios@gmail.com>
2023-11-13 12:40:55 -0600

    tarjeta de juego destacado

--- src/app/screens/tienda/tienda.component.ts
+++ src/app/screens/tienda/tienda.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import juego from '../../interfaces/juego';
 
 @Component({
   selector: 'app-tienda',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent {
+  JuegosDestacado: juego[] = [];
+  IndiceDestacado: number= 0;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.JuegosDestacado = [
+      {
+        id: 1,
+        nombre: 'Doom (1993)',
+        descripcion: 'Mejor shooter de la historia',
+        precio: 200,
+        descuento: 10,
+        calificacion: 10,
+        esbr: "T",
+        fecha_lanzamiento: "1993-10-01",
+        imagen_fondo: "../../assets/images/Doom_fondo.jpg",
+        imagen_portada: "../../assets/images/Doom_portada.jfif",
+      }
+    ]
+  }
 }
