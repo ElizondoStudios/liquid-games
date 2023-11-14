e30f0d3e9db7d497c4a600a20726b164592ecc87
José Luis ELizondo <elizondostudios@gmail.com>
2023-11-14 10:01:26 -0600

    Carrousell de juegos destacados

--- src/app/screens/tienda/tienda.component.ts
+++ src/app/screens/tienda/tienda.component.ts
@@ -13,6 +13,42 @@ export class TiendaComponent {
   constructor() { }
 
   ngOnInit(): void {
+    this.GetJuegosDestacado();
+  }
+
+  siguienteJuego(): void {
+    if(this.IndiceDestacado===this.JuegosDestacado.length-1)
+      this.IndiceDestacado=0;
+    else
+      this.IndiceDestacado++;
+    const infoJuego= document.querySelector('.info-juego');
+    infoJuego?.classList.add("transicion-juego-siguiente");
+    const TO= setTimeout(() => {
+      infoJuego?.classList.remove("transicion-juego-siguiente");
+      clearTimeout(TO);
+    }
+    , 600);
+  }
+
+  anteriorJuego(): void {
+    if(this.IndiceDestacado===0)
+      this.IndiceDestacado=this.JuegosDestacado.length-1;
+    else
+      this.IndiceDestacado--;
+      const infoJuego= document.querySelector('.info-juego');
+      infoJuego?.classList.add("transicion-juego-anterior");
+      const TO= setTimeout(() => {
+        infoJuego?.classList.remove("transicion-juego-anterior");
+        clearTimeout(TO);
+      }
+      , 600);
+  }
+
+  formatCurrency(cantidad: number): string{
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'MXN' }).format(cantidad);
+  }
+
+  GetJuegosDestacado(): void {
     this.JuegosDestacado = [
       {
         id: 1,
@@ -25,6 +61,18 @@ export class TiendaComponent {
         fecha_lanzamiento: "1993-10-01",
         imagen_fondo: "../../assets/images/Doom_fondo.jpg",
         imagen_portada: "../../assets/images/Doom_portada.jfif",
+      },
+      {
+        id: 1,
+        nombre: 'Ivancito Georgito',
+        descripcion: 'UwU 👉👈',
+        precio: 2000000,
+        descuento: 10,
+        calificacion: 10,
+        esbr: "M",
+        fecha_lanzamiento: "1993-10-01",
+        imagen_fondo: "../../assets/images/ivancito_georgi.jfif",
+        imagen_portada: "../../assets/images/ivan.jfif",
       }
     ]
   }
