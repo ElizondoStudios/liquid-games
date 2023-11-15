db420f000307f2d6347ee817a8bf9e9d117d3e87
José Luis ELizondo <elizondostudios@gmail.com>
2023-11-15 08:21:33 -0600

    Merge remote-tracking branch 'origin/login' into Tienda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './layouts/inicio/inicio.component';
@@ -8,8 +7,10 @@ import { TiendaComponent } from './screens/tienda/tienda.component';
 import { LoginComponent } from './screens/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BibliotecaComponent } from './screens/biblioteca/biblioteca.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { CarritoComponent } from './screens/carrito/carrito.component';
 
 @NgModule({
@@ -19,7 +20,7 @@ import { CarritoComponent } from './screens/carrito/carrito.component';
     TiendaComponent,
     LoginComponent,
     BibliotecaComponent,
-    CarritoComponent
+    CarritoComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,8 +28,10 @@ import { CarritoComponent } from './screens/carrito/carrito.component';
     NgbModule,
     MatIconModule,
     MatTooltipModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
