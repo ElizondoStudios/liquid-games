d12fa881be0708a0d7a39e40112c5aa7a1235726
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-03 13:49:34 -0600

    base: user service te permite guardar la id del ususario para el interceptor

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,10 +10,14 @@ export class UserService {
   idUsuario: number | null = null;
 
   SetUsuarioID(id: number | null): void{
+    localStorage.setItem('idUsuario', id?.toString() ?? '');
     this.idUsuario = id;
   }
 
   GetUsuarioID(): number | null{
+    if(this.idUsuario == null){
+      this.idUsuario = parseInt(localStorage.getItem('idUsuario') ?? '') || null;
+    }
     return this.idUsuario;
   }
 }
