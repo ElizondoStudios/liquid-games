1727e991d6759efba6c06a2fb3884d46a7db3aae
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-03 15:35:51 -0600

    base: end points signin fix: anchor en login

--- src/app/screens/sign-in/sign-in.component.ts
+++ src/app/screens/sign-in/sign-in.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorComponent } from 'src/app/components/error/error.component';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,6 +13,34 @@ import { Component } from '@angular/core';
 })
 export class SignInComponent {
   hoy = new Date();
+  Signform: FormGroup;
 
-  constructor() {}
+  constructor( private formBuilder: FormBuilder, private user: UserService,
+    private api: ApiService, private router: Router, private dialog: MatDialog) {
+   this.Signform= this.createSignForm();
+  }
+
+  createSignForm(): FormGroup{
+    return this.formBuilder.group({
+      usuario: ['', Validators.required],
+      contrasenia: ['', [Validators.required, Validators.minLength(8)]],
+      correo: ['', [Validators.required, Validators.email]],
+      fechaNacimiento: ['', Validators.required]
+    });
+  }
+
+  SignIn(){
+    if(this.Signform.valid){
+      this.api.postUsuario(this.Signform?.value).subscribe(
+        res => {
+          this.user.SetUsuarioID(res.id);
+          this.router.navigate(['/inicio']);
+        },
+        err => {
+          console.log(err)
+          this.dialog.open(ErrorComponent);
+        }
+      );
+    }
+  }
 }
