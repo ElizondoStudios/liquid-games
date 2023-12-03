cda5558f93c3bc6f8087370861a03b006ee9e7c2
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-03 17:27:14 -0600

    Pipe en interceptor

--- src/app/interceptor/interceptor.interceptor.ts
+++ src/app/interceptor/interceptor.interceptor.ts
@@ -16,7 +16,8 @@ export class InterceptorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if(this.user.GetUsuarioID() != null || request.url.includes("/Login/postTryLogin") || request.url.includes("/Usuarios/post")){
-      return next.handle(request);
+      // return next.handle(request);
+      return next.handle(request).pipe();
     }else{
       this.router.navigateByUrl('/login');
       return new Observable<HttpEvent<unknown>>();
