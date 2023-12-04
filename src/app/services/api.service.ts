150554de53eb7977ff3c955477bc2d58b38d1d1a
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-04 12:01:10 -0600

    fix: Usuario y api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -98,7 +98,7 @@ export class ApiService {
   }
 
   public putUsuario(params: {id: number, usuario : string, contrasenia : string, correo : string, fechaNacimiento : string}): Observable<any>{
-    return this.http.post(`${environment.API_URL}/Usuarios/putUsuario`, params)
+    return this.http.put(`${environment.API_URL}/Usuarios/putUsuario`, params)
     //Post par poder crear un nuevo usuario
     //route: /api/Usuarios/postUsuario
   }
