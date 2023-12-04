e84350e1398a68ccbaed486822fb9259b870a76b
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-03 18:33:56 -0600

    fix: venta-juego valida si el usuario tiene el juego

--- src/app/screens/venta-juego/venta-juego.component.ts
+++ src/app/screens/venta-juego/venta-juego.component.ts
@@ -24,6 +24,7 @@ export class VentaJuegoComponent implements OnInit{
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.GetJuegoPorId(params['id']);
+      this.VerificarJuegoEnBiblioteca();
     });
   }
 
